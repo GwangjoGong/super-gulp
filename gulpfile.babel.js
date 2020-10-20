b5178c231d3a894b8732d5c5be0a88c102018e38
Gwangjo Gong <dwc05101@kaist.ac.kr>
2020-10-20 18:28:59 +0900

    pug compilation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,17 @@
 import gulp from 'gulp'
+import pugPlugin from 'gulp-pug'
+import del from 'del'
+
+const routes = {
+  pug: {
+    src: 'src/*.pug',
+    dest: 'build'
+  }
+}
+
+export const pug = () =>
+  gulp.src(routes.pug.src).pipe(pugPlugin()).pipe(gulp.dest(routes.pug.dest))
+
+export const clean = () => del(['build'])
+
+export const dev = gulp.series([clean, pug])
