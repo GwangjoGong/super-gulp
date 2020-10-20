2c444528f9015f8327ffec67e94def361ee1519c
Gwangjo Gong <dwc05101@kaist.ac.kr>
2020-10-20 18:47:36 +0900

    image optimization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 import gulp from 'gulp'
 import pugPlugin from 'gulp-pug'
+import imagePlugin from 'gulp-image'
 import del from 'del'
 import ws from 'gulp-webserver'
 
@@ -8,24 +9,35 @@ const routes = {
     watch: 'src/**/*.pug',
     src: 'src/*.pug',
     dest: 'build'
+  },
+  images: {
+    src: 'src/img/*',
+    dest: 'build/img'
   }
 }
+
+const clean = () => del(['build'])
 
 const pug = () =>
   gulp.src(routes.pug.src).pipe(pugPlugin()).pipe(gulp.dest(routes.pug.dest))
 
-const clean = () => del(['build'])
+const img = () =>
+  gulp
+    .src(routes.images.src)
+    .pipe(imagePlugin())
+    .pipe(gulp.dest(routes.images.dest))
 
 const webserver = () =>
   gulp.src('build').pipe(ws({ livereload: true, open: true }))
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug)
+  gulp.watch(routes.images.src, img)
 }
 
 export const prepare = gulp.series([clean])
 
-export const assets = gulp.series([pug])
+export const assets = gulp.series([pug, img])
 
 export const postDev = gulp.parallel([webserver, watch])
 
